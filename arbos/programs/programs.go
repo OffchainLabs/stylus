caf36d9f4d8e006d521adcfc1d6904b1fe02f254
Joshua Colvin <jcolvin@offchainlabs.com>
2023-05-10 20:48:23 -0700

    Pack wasm program settings

--- arbos/programs/programs.go
+++ arbos/programs/programs.go
@@ -4,7 +4,7 @@
 package programs
 
 import (
-	"errors"
+	"encoding/binary"
 	"fmt"
 
 	"github.com/ethereum/go-ethereum/common"
@@ -18,85 +18,146 @@ import (
 	"github.com/offchainlabs/nitro/util/arbmath"
 )
 
-const MaxWasmSize = 64 * 1024
+const (
+	ByteOffsetVersion       = 0
+	ByteOffsetInkPrice      = 4
+	ByteOffsetWasmMaxDepth  = 12
+	ByteOffsetWasmHostioInk = 16
+	MaxWasmSize             = 64 * 1024
+)
+
+type Settings struct {
+	Version       uint32
+	InkPrice      arbmath.UBips
+	WasmMaxDepth  uint32
+	WasmHostioInk uint64
+}
+
+func (s *Settings) serialize() []byte {
+	data := make([]byte, 24)
+	binary.BigEndian.PutUint32(data[ByteOffsetVersion:], s.Version)
+	binary.BigEndian.PutUint64(data[ByteOffsetInkPrice:], uint64(s.InkPrice))
+	binary.BigEndian.PutUint32(data[ByteOffsetWasmMaxDepth:], s.WasmMaxDepth)
+	binary.BigEndian.PutUint64(data[ByteOffsetWasmHostioInk:], s.WasmHostioInk)
+
+	return data
+}
+
+func deserializeSettings(data []byte) *Settings {
+	var settings Settings
+	settings.Version = binary.BigEndian.Uint32(data[ByteOffsetVersion:])
+	settings.InkPrice = arbmath.UBips(binary.BigEndian.Uint64(data[ByteOffsetInkPrice:]))
+	settings.WasmMaxDepth = binary.BigEndian.Uint32(data[ByteOffsetWasmMaxDepth:])
+	settings.WasmHostioInk = binary.BigEndian.Uint64(data[ByteOffsetWasmHostioInk:])
+
+	return &settings
+}
 
 type Programs struct {
 	backingStorage  *storage.Storage
 	machineVersions *storage.Storage
-	inkPrice        storage.StorageBackedUBips
-	wasmMaxDepth    storage.StorageBackedUint32
-	wasmHostioInk   storage.StorageBackedUint64
-	version         storage.StorageBackedUint32
 }
 
 var machineVersionsKey = []byte{0}
-
-const (
-	versionOffset uint64 = iota
-	inkPriceOffset
-	wasmMaxDepthOffset
-	wasmHostioInkOffset
-)
 
 var ProgramNotCompiledError func() error
 var ProgramOutOfDateError func(version uint32) error
 var ProgramUpToDateError func() error
 
-func Initialize(sto *storage.Storage) {
-	inkPrice := sto.OpenStorageBackedBips(inkPriceOffset)
-	wasmMaxDepth := sto.OpenStorageBackedUint32(wasmMaxDepthOffset)
-	wasmHostioInk := sto.OpenStorageBackedUint32(wasmHostioInkOffset)
-	version := sto.OpenStorageBackedUint64(versionOffset)
-	_ = inkPrice.Set(1)
-	_ = wasmMaxDepth.Set(math.MaxUint32)
-	_ = wasmHostioInk.Set(0)
-	_ = version.Set(1)
+func Initialize(sto *storage.Storage) error {
+	settings := Settings{
+		Version:       1,
+		InkPrice:      1,
+		WasmMaxDepth:  math.MaxUint32,
+		WasmHostioInk: 0,
+	}
+	return sto.SetBytes(settings.serialize())
 }
 
 func Open(sto *storage.Storage) *Programs {
 	return &Programs{
 		backingStorage:  sto,
 		machineVersions: sto.OpenSubStorage(machineVersionsKey),
-		inkPrice:        sto.OpenStorageBackedUBips(inkPriceOffset),
-		wasmMaxDepth:    sto.OpenStorageBackedUint32(wasmMaxDepthOffset),
-		wasmHostioInk:   sto.OpenStorageBackedUint64(wasmHostioInkOffset),
-		version:         sto.OpenStorageBackedUint32(versionOffset),
 	}
+}
+
+func (p Programs) Settings() (*Settings, error) {
+	bytes, err := p.backingStorage.GetBytes()
+	if err != nil {
+		return nil, err
+	}
+	return deserializeSettings(bytes), nil
+}
+
+func (p Programs) SaveSettings(settings *Settings) error {
+	return p.backingStorage.SetBytes(settings.serialize())
 }
 
 func (p Programs) StylusVersion() (uint32, error) {
-	return p.version.Get()
+	settings, err := p.Settings()
+	if err != nil {
+		return 0, err
+	}
+	return settings.Version, nil
 }
 
 func (p Programs) InkPrice() (arbmath.UBips, error) {
-	return p.inkPrice.Get()
+	settings, err := p.Settings()
+	if err != nil {
+		return 0, err
+	}
+	return settings.InkPrice, nil
 }
 
 func (p Programs) SetInkPrice(price arbmath.UBips) error {
-	if price == 0 {
-		return errors.New("ink price must be nonzero")
+	settings, err := p.Settings()
+	if err != nil {
+		return err
 	}
-	return p.inkPrice.Set(price)
+	settings.InkPrice = price
+	return p.SaveSettings(settings)
 }
 
 func (p Programs) WasmMaxDepth() (uint32, error) {
-	return p.wasmMaxDepth.Get()
+	settings, err := p.Settings()
+	if err != nil {
+		return 0, err
+	}
+	return settings.WasmMaxDepth, nil
 }
 
 func (p Programs) SetWasmMaxDepth(depth uint32) error {
-	return p.wasmMaxDepth.Set(depth)
+	settings, err := p.Settings()
+	if err != nil {
+		return err
+	}
+	settings.WasmMaxDepth = depth
+	return p.SaveSettings(settings)
 }
 
 func (p Programs) WasmHostioInk() (uint64, error) {
-	return p.wasmHostioInk.Get()
+	settings, err := p.Settings()
+	if err != nil {
+		return 0, err
+	}
+	return settings.WasmHostioInk, nil
 }
 
 func (p Programs) SetWasmHostioInk(cost uint64) error {
-	return p.wasmHostioInk.Set(cost)
+	settings, err := p.Settings()
+	if err != nil {
+		return err
+	}
+	settings.WasmHostioInk = cost
+	return p.SaveSettings(settings)
 }
 
 func (p Programs) ProgramVersion(program common.Address) (uint32, error) {
-	return p.machineVersions.GetUint32(program.Hash())
+	settings, err := p.Settings()
+	if err != nil {
+		return 0, err
+	}
+	return settings.Version, nil
 }
 
 func (p Programs) CompileProgram(statedb vm.StateDB, program common.Address, debugMode bool) (uint32, error) {
@@ -175,23 +236,15 @@ type goParams struct {
 }
 
 func (p Programs) goParams(version uint32, debug bool) (*goParams, error) {
-	maxDepth, err := p.WasmMaxDepth()
-	if err != nil {
-		return nil, err
-	}
-	inkPrice, err := p.InkPrice()
-	if err != nil {
-		return nil, err
-	}
-	hostioInk, err := p.WasmHostioInk()
+	settings, err := p.Settings()
 	if err != nil {
 		return nil, err
 	}
 	config := &goParams{
-		version:   version,
-		maxDepth:  maxDepth,
-		inkPrice:  inkPrice.Uint64(),
-		hostioInk: hostioInk,
+		version:   settings.Version,
+		maxDepth:  settings.WasmMaxDepth,
+		inkPrice:  settings.InkPrice.Uint64(),
+		hostioInk: settings.WasmHostioInk,
 	}
 	if debug {
 		config.debugMode = 1
