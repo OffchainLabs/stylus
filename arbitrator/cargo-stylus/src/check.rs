47bc79719bdcc5679e75cf6cdf4841b274add824
Raul Jordan <raul@prysmaticlabs.com>
2023-07-21 15:45:36 -0400

    begin deploy

--- arbitrator/cargo-stylus/src/check.rs
+++ arbitrator/cargo-stylus/src/check.rs
@@ -1,13 +1,13 @@
 use std::env::current_dir;
 use std::io::Read;
 use std::path::PathBuf;
-use std::process::Command;
+use std::process::{Command, Stdio};
 
 use brotli2::read::BrotliEncoder;
 use bytes::buf::Reader;
 use bytes::Buf;
 use bytesize::ByteSize;
-use hex;
+use eyre::bail;
 
 use arbutil::Color;
 use prover::programs::prelude::*;
@@ -34,20 +34,23 @@ impl From<&str> for StylusCheck {
 }
 
 pub fn run_checks(disabled: Option<Vec<StylusCheck>>) -> eyre::Result<()> {
-    let cwd: PathBuf = current_dir().unwrap();
-
     // Compile the Rust program at the current working directory into WASM using
     // Cargo and then instrument the WASM code with Stylus. If any of the checks
     // are disabled, we avoid runnng it.
-    let _check_compressed_size = disabled
+    let check_compressed_size = disabled
         .as_ref()
         .map(|d: &Vec<StylusCheck>| !d.contains(&StylusCheck::CompressedSize))
         .unwrap_or(true);
 
+    let cwd: PathBuf = current_dir().unwrap();
+
     // TODO: Configure debug or release via flags.
     // TODO: Capture errors from this command.
     Command::new("cargo")
+        .stdout(Stdio::inherit())
+        .stderr(Stdio::inherit())
         .arg("build")
+        .arg("--release")
         .arg("--target=wasm32-unknown-unknown")
         .output()
         .expect("Failed to execute cargo build");
@@ -55,7 +58,7 @@ pub fn run_checks(disabled: Option<Vec<StylusCheck>>) -> eyre::Result<()> {
     let wasm_path = cwd
         .join("target")
         .join("wasm32-unknown-unknown")
-        .join("debug")
+        .join("release")
         .join(format!("{}.wasm", "echo"));
 
     println!("Reading compiled WASM at {}", wasm_path.display().yellow());
@@ -65,36 +68,59 @@ pub fn run_checks(disabled: Option<Vec<StylusCheck>>) -> eyre::Result<()> {
 
     let wbytes: Reader<&[u8]> = wasm_file_bytes.reader();
 
-    // TODO: Configure compression level, move to constants.
+    // TODO: Configure compression level.
     let mut compressor = BrotliEncoder::new(wbytes, constants::BROTLI_COMPRESSION_LEVEL);
     let mut compressed_bytes = vec![];
     compressor.read_to_end(&mut compressed_bytes).unwrap();
 
     println!(
-        "Uncompressed size: {}",
+        "Uncompressed WASM size: {}",
         ByteSize::b(wasm_file_bytes.len() as u64)
             .to_string()
             .yellow(),
     );
+    
+    let compressed_size = ByteSize::b(compressed_bytes.len() as u64);
 
-    println!(
-        "Brotli compressed size: {}",
-        ByteSize::b(compressed_bytes.len() as u64)
-            .to_string()
-            .mint(),
-    );
+    if check_compressed_size {
+        // TODO: Configure.
+        if compressed_size > ByteSize::kb(24) {
+            bail!(
+                "Brotli-compressed WASM size {} is bigger than program size limit: {}",
+                compressed_size.to_string().red(),
+                ByteSize::kb(24).to_string(),
+            );
+        } else {
+            println!(
+                "Brotli-compressed WASM size {} within program size limit: {}",
+                compressed_size.to_string().mint(),
+                ByteSize::kb(24).to_string(),
+            );
+        }
+    }
 
     let config = CompileConfig::default();
-    let instrumented = stylus::native::module(
-        &wasm_file_bytes, config
-    ).unwrap();
+    let module = match stylus::native::module(&wasm_file_bytes, config) {
+        Ok(module) => module,
+        Err(error) => {
+            bail!("Failed to compile WASM: {:?}", error);
+        }
+    };
 
+    let success = "Stylus compilation successful!".to_string().mint();
+    println!("{}", success);
     println!(
-        "Instrumented wasm raw bytes: {}",
-        ByteSize::b(instrumented.len() as u64)
+        "Compiled WASM module total size: {}",
+        ByteSize::b(module.len() as u64)
             .to_string()
-            .mint(),
     );
+    Ok(())
+}
 
+pub fn check_compressed_size() -> eyre::Result<()> {
+    Ok(())
+}
+
+pub fn check_compilation() -> eyre::Result<()> {
     Ok(())
 }
--- arbitrator/cargo-stylus/src/deploy.rs
+++ arbitrator/cargo-stylus/src/deploy.rs
@@ -1,1 +1,49 @@
+use std::env::current_dir;
+use std::io::Read;
+use std::path::PathBuf;
+use std::process::{Command, Stdio};
 
+use brotli2::read::BrotliEncoder;
+use bytes::buf::Reader;
+use bytes::Buf;
+
+use arbutil::Color;
+
+use crate::constants;
+
+pub fn deploy_and_compile_onchain() -> eyre::Result<()> {
+    let cwd: PathBuf = current_dir().unwrap();
+
+    // TODO: Configure debug or release via flags.
+    // TODO: Capture errors from this command.
+    Command::new("cargo")
+        .stdout(Stdio::inherit())
+        .stderr(Stdio::inherit())
+        .arg("build")
+        .arg("--release")
+        .arg("--target=wasm32-unknown-unknown")
+        .output()
+        .expect("Failed to execute cargo build");
+
+    let wasm_path = cwd
+        .join("target")
+        .join("wasm32-unknown-unknown")
+        .join("release")
+        .join(format!("{}.wasm", "echo"));
+
+    println!("Reading compiled WASM at {}", wasm_path.display().yellow());
+
+    let wasm_file_bytes =
+        std::fs::read(&wasm_path).expect("Could not read WASM file at target path");
+
+    let wbytes: Reader<&[u8]> = wasm_file_bytes.reader();
+
+    let mut compressor = BrotliEncoder::new(wbytes, constants::BROTLI_COMPRESSION_LEVEL);
+    let mut compressed_bytes = vec![];
+    compressor.read_to_end(&mut compressed_bytes).unwrap();
+
+    // Next, we prepend with the EOF bytes and prepare a compilation tx onchain. Uses ethers
+    // to prepare the tx and send it over onchain to an endpoint. Will prepare a multicall data
+    // tx to send to a multicall.rs rust program.
+    Ok(())
+}
