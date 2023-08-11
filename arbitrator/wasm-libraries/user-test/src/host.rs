8840d9021c5b588deacb897779d2af91fb73f89e
Joshua Colvin <jcolvin@offchainlabs.com>
2023-08-11 16:30:42 -0700

    Fix unit tests

--- arbitrator/wasm-libraries/user-test/src/host.rs
+++ arbitrator/wasm-libraries/user-test/src/host.rs
@@ -64,13 +64,13 @@ pub unsafe extern "C" fn vm_hooks__emit_log(data: usize, len: u32, topics: u32) 
 
 #[no_mangle]
 pub unsafe extern "C" fn vm_hooks__report_hostio(_opcode: u32, _gas: u64, _cost: u64) {
-    let mut _program = Program::start();
+    let mut _program = Program::start_free();
     // Do nothing
 }
 
 #[no_mangle]
 pub unsafe extern "C" fn vm_hooks__report_hostio_advanced(_opcode: u32, data: usize, len: u32, _offset: u32, _size: u32, _gas: u64, _cost: u64) {
-    let mut _program = Program::start();
+    let mut _program = Program::start_free();
     let _data = wavm::read_slice_usize(data, len as usize);
     // Do nothing
 }
